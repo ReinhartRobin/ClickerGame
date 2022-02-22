895b5c60e63177d14c0aaf2bce206d32fe68bcea
reini-java <robinreinhart46@gmail.com>
2022-02-22 14:55:45 +0100

    Working on backend

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ let value = 1
 
 function addPoint() {
     points += value;
-    console.log(value + '|' + points);
     document.querySelector("#points").textContent = "Points: " + points;
     checkShop();
+    let click = document.createElement("p");
+    let clickText = document.createTextNode("+1");
+    click.appendChild(clickText);
+    click.style = "position:absolute; left:" + Math.random() * 90 + "%; top:" + Math.random() * 90 + "%;";
+    document.querySelector(".Clicker").appendChild(click);
+    setTimeout(() => {
+        document.querySelector(".Clicker").removeChild(click);
+    }, 1000);
 }
 
 function checkShop() {
-    if (points >= 10) {
+    if (points >= 500) {
         document.getElementById("doubleButton").classList.remove("disabledButton");
 
         document.getElementById("doubleClick").innerHTML = `
@@ -21,11 +28,13 @@ function checkShop() {
                         </span></button>
         `;
         document.getElementById("doubleButton").classList.add("enabledButton");
+    } else {
+        document.getElementById("doubleButton").classList.add("disabledButton");
     }
-    if (points >= 15) {
+    if (points >= 1000) {
         document.getElementById("addModelButton").classList.remove("disabledButton");
         document.getElementById("addModel").innerHTML = `
-        <button id="addModelButton"><span class="fa fa-certificate"></span>
+        <button onclick="addModel()" id="addModelButton"><span class="fa fa-certificate"></span>
                         &nbsp;
                         add model
                         <span class="btn-overlay">
@@ -33,12 +42,14 @@ function checkShop() {
                         </span></button>
         `;
         document.getElementById("addModelButton").classList.add("enabledButton");
+    } else {
+        document.getElementById("addModelButton").classList.add("disabledButton");
     }
 
-    if (points >= 20) {
+    if (points >= 2500) {
         document.getElementById("addSuperModelButton").classList.remove("disabledButton");
         document.getElementById("addSuperModel").innerHTML = `
-        <button id="addSuperModelButton"><span class="fa fa-certificate"></span>
+        <button onclick="addSuperModel()" id="addSuperModelButton"><span class="fa fa-certificate"></span>
                         &nbsp;
                         add supermodel
                         <span class="btn-overlay">
@@ -46,12 +57,14 @@ function checkShop() {
                         </span></button>
         `;
         document.getElementById("addSuperModelButton").classList.add("enabledButton");
+    } else {
+        document.getElementById("addSuperModelButton").classList.add("disabledButton");
     }
 
-    if (points >= 25) {
+    if (points >= 5000) {
         document.getElementById("addMegaModelButton").classList.remove("disabledButton");
         document.getElementById("addMegaModel").innerHTML = `
-        <button id="addMegaModelButton"><span class="fa fa-certificate"></span>
+        <button onclick="addMegaModel()" id="addMegaModelButton"><span class="fa fa-certificate"></span>
                         &nbsp;
                         add megamodel
                         <span class="btn-overlay">
@@ -59,16 +72,51 @@ function checkShop() {
                         </span></button>
         `;
         document.getElementById("addMegaModelButton").classList.add("enabledButton");
+    } else {
+        document.getElementById("addMegaModelButton").classList.add("disabledButton");
     }
+
 }
 
 function doubleClicks() {
-    alert("hello");
     if (document.getElementById("doubleButton").className === "enabledButton") {
-        if (value === 1) {
-            value++
-        } else {
-            value *= 2;
-        }
+        value === 1 ? value++ : value *= 2;
+        buy(500);
     }
+}
+
+function addModel() {
+    if (document.getElementById("addModelButton").className === "enabledButton") {
+        setInterval(function() {
+            points++;
+            document.querySelector("#points").textContent = "Points: " + points;
+        }, 1000);
+        buy(1000);
+    }
+}
+
+function addSuperModel() {
+    if (document.getElementById("addModelButton").className === "enabledButton") {
+        setInterval(function() {
+            points += 2;
+            document.querySelector("#points").textContent = "Points: " + points;
+        }, 1000);
+        buy(2500);
+    }
+}
+
+function addMegaModel() {
+    if (document.getElementById("addModelButton").className === "enabledButton") {
+        setInterval(function() {
+            points += 5;
+            document.querySelector("#points").textContent = "Points: " + points;
+        }, 1000);
+        buy(5000);
+    }
+}
+
+function buy(costs) {
+    points -= costs;
+    document.querySelector("#points").textContent = "Points: " + points;
+    checkShop();
 }
